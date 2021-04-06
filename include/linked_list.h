38689915375e5d33edebf41d1adccb2a50aa7d46
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-04-07 00:29:04 +0300

    LinkedList: header definition and linkedListCreate impl

---
+++ include/linked_list.h
@@ -0,0 +1,38 @@
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+
+
+typedef struct __linked_list LinkedList;
+
+typedef void* (*copy_func_t)(const void* element);
+typedef void (*free_func_t)(void* element);
+typedef int (*cmp_func_t)(const void* first, const void* second);
+
+typedef enum {
+    LINKED_LIST_SUCCESS,
+    LINKED_LIST_MEM_ERROR
+
+} LinkedListStatus;
+
+
+LinkedList* linkedListCreate(copy_func_t copy_func, free_func_t free_func, cmp_func_t compare_func);
+LinkedList* linkedListClone(const LinkedList* list);
+void linkedListDestroy(LinkedList* list);
+
+size_t linkedListSize(const LinkedList* list);
+size_t linkedListIndexOf(const LinkedList* list, const void* element);
+
+void* linkedListGetAt(const LinkedList* list, size_t index);
+void* linkedListRemoveAt(LinkedList* list, size_t index);
+
+LinkedListStatus linkedListInsertBefore(LinkedList* list, void* anchor, const void* new_element);
+LinkedListStatus linkedListInsertAfter(LinkedList* list, void* anchor, const void* new_element);
+
+LinkedListStatus linkedListPush(LinkedList* list, void* element);
+void* linkedListPop(LinkedList* list, LinkedListStatus* status);
+
+LinkedListStatus linkedListPushFront(LinkedList* list, void* element);
+void* linkedListPopFront(LinkedList* list, LinkedListStatus* status);
+
+
+#endif // __LINKED_LIST_H__
---
+++ src/linked_list.c
@@ -0,0 +1,59 @@
+#include <assert.h>
+#include <malloc.h>
+
+#include "linked_list.h"
+
+typedef struct node {
+    void* data;
+
+    struct node* next;
+    struct node* prev;
+
+} LinkedListNode;
+
+static LinkedListNode* nodeCreate(void* data) {
+    LinkedListNode* new_node = malloc(sizeof(*new_node));
+    if (new_node) {
+        new_node->data = data;
+        new_node->next = new_node->prev = NULL;
+    }
+
+    return new_node;
+}
+
+
+struct __linked_list {
+    LinkedListNode* head;
+    LinkedListNode* tail;
+
+    copy_funct_t copy;
+    cmp_func_t compare;
+    free_func_t free;
+};
+
+
+LinkedList* linkedListCreate(copy_func_t copy_func, free_func_t free_func, cmp_func_t compare_func) {
+    assert(copy_func); assert(free_func); assert(compare_func);
+
+    LinkedList* new_list = malloc(sizeof(*new_list));
+    if (new_list) {
+        new_list->data = NULL;
+        new_list->head = nodeCreate(NULL);
+        if (NULL == new_list->head) {
+            linkedListDestroy(new_list);
+            return NULL;
+        }
+
+        new_list->tail = nodeCreate(NULL);
+        if (NULL == new_list->tail) {
+            linkedListDestroy(new_list);
+            return NULL;
+        }
+
+        // both allocations succeeded
+        new_list->head->next = new_list->tail;
+        new_list->tail->prev = new_list->head;
+    }
+
+    return new_list;
+}
