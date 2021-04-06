2f796e48c2c88cc258dcf2c1bf21c0c6356dc654
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-04-07 00:40:42 +0300

    linkedListDestroy

--- src/linked_list.c
+++ src/linked_list.c
@@ -21,6 +21,13 @@ static LinkedListNode* nodeCreate(void* data) {
     return new_node;
 }
 
+static void nodeDestroy(LinkedListNode* node, free_func_t free_func) {
+    if (node) {
+        if (free_func) free_func(node->data);
+        free(node);
+    }
+}
+
 
 struct __linked_list {
     LinkedListNode* head;
@@ -38,6 +45,7 @@ LinkedList* linkedListCreate(copy_func_t copy_func, free_func_t free_func, cmp_f
     LinkedList* new_list = malloc(sizeof(*new_list));
     if (new_list) {
         new_list->data = NULL;
+        
         new_list->head = nodeCreate(NULL);
         if (NULL == new_list->head) {
             linkedListDestroy(new_list);
@@ -56,4 +64,21 @@ LinkedList* linkedListCreate(copy_func_t copy_func, free_func_t free_func, cmp_f
     }
 
     return new_list;
+}
+
+void linkedListDestroy(LinkedList* list) {
+    if (list) {
+        LinkedListNode* node_itr = list->head->next;
+        while (node_itr != list->tail) {
+            LinkedListNode* current = node_itr;
+            node_itr = node_itr->next;
+
+            nodeDestroy(current, list->free);
+        }
+        
+        nodeDestroy(list->head, NULL);
+        nodeDestroy(list->tail, NULL);
+
+        free(list);
+    }
 }
